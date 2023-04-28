8446106b39ea3c781265d9500484949054b0db79
gael-gothuey <gothuey.gael@gmail.com>
2023-04-28 22:36:50 +0200

    Enable Giscus comment

--- src/lib/config/post.ts
+++ src/lib/config/post.ts
@@ -1,3 +1,18 @@
 import type { PostConfig } from '$lib/types/post'
 
-export const post: PostConfig = {}
+export const post: PostConfig = {
+    comment: {
+        use: ['Giscus'],
+        giscus: {
+            repo: 'gaelgoth/blog',
+            repoID: 'R_kgDOHydF9Q',
+            category: 'Comments',
+            categoryID: 'DIC_kwDOHydF9c4CWIYO',
+            reactionsEnabled: true,
+            inputPosition: 'top',
+            lang: 'en',
+            theme: 'preferred_color_scheme',
+            loading: 'lazy'
+        }
+    }
+}
