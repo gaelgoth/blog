256d8093ffe208169d1097f02afb7c7c9528414c
gaelgoth <gothuey.gael@gmail.com>
2025-02-09 15:54:40 +0100

    fix: giscus ids

--- src/lib/config/post.ts
+++ src/lib/config/post.ts
@@ -1,18 +1,18 @@
 import type { PostConfig } from '$lib/types/post'
 
 export const post: PostConfig = {
-    comment: {
-        use: ['Giscus'],
-        giscus: {
-            repo: 'gaelgoth/blog',
-            repoID: 'R_kgDOHydF9Q',
-            category: 'Comments',
-            categoryID: 'DIC_kwDOHydF9c4CWIYO',
-            reactionsEnabled: true,
-            inputPosition: 'top',
-            lang: 'en',
-            theme: 'preferred_color_scheme',
-            loading: 'lazy'
-        }
+  comment: {
+    use: ['Giscus'],
+    giscus: {
+      repo: 'gaelgoth/blog',
+      repoID: 'R_kgDONvmwGA',
+      category: 'Comments',
+      categoryID: 'DIC_kwDONvmwGM4CmzXe',
+      reactionsEnabled: true,
+      inputPosition: 'top',
+      lang: 'en',
+      theme: 'preferred_color_scheme',
+      loading: 'lazy'
     }
+  }
 }
